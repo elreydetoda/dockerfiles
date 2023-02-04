08c0197a8c14a39aa51a26721cf00f6764c12100
elreydetoda <2417550-elreydetoda@users.noreply.gitlab.com>
2023-02-04 18:22:02 -0500

    adding packer vmware dockerfile and entrypoint

---
+++ packer-vmware/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# https://elrey.casa/bash/scripting/harden
+set -${-//[sc]/}eu${DEBUG:+xv}o pipefail
+
+function ubuntu_prep(){
+  # installing current running kernel stuff
+  apt-get update
+  apt-get install -y "linux-headers-$(uname -r)" "linux-image-$(uname -r)"
+}
+
+function vmware_prep(){
+  echo "Setting up VMWare"
+  /usr/bin/vmware-modconfig --console --install-all
+  /etc/init.d/vmware start
+  /etc/init.d/vmware-workstation-server start
+  /etc/init.d/vmware-USBArbitrator start
+}
+
+function main(){
+  VMWARE_SN="${VMWARE_LIC_KEY:-}"
+
+  if [[ -n "${VMWARE_SN}" ]] ; then
+    ubuntu_prep
+    vmware_prep
+    /usr/lib/vmware/bin/vmware-vmx --new-sn "${VMWARE_SN}"
+    packer "${@}"
+  else
+    echo "Please set your VMWare licence as an environment variable of VMWARE_LIC_KEY"
+    echo " (i.e. VMWARE_LIC_KEY='xxxxx-xxxxx-xxxxx-xxxxx-xxxxx')"
+    exit 1
+  fi
+}
+
+# https://elrey.casa/bash/scripting/main
+if [[ "${0}" = "${BASH_SOURCE[0]:-bash}" ]] ; then
+  main "${@}"
+fi
